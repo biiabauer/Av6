a2ac81a14ace43689caa8a56fe18d819732d4e35
unknown <biia.bauer16@gmail.com>
2020-12-08 11:30:36 -0300

    Codigo pronto

---
+++ modelo.py
@@ -0,0 +1,135 @@
+from config import *
+
+class Livro(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    autor = db.Column(db.String(254))
+    ano_publi = db.Column(db.String(254))
+
+    def __str__(self):
+        return self.nome + "[id="+str(self.id)+ "], " +\
+            self.autor + ", " + self.ano_publi
+            
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "autor": self.autor,
+            "ano_publi": self.ano_publi
+        }
+
+class Leitor(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254)) 
+    
+    livro_id = db.Column(db.Integer, db.ForeignKey(Livro.id), nullable=False) 
+    #NULLABLE SERVE PARA QUANDO É DEPENDENTE UM DO OUTRO
+    livro = db.relationship("Livro")
+
+    def __str__(self):
+        return  f"{self.nome} [{self.id}]"
+
+    def json(self):
+        return {
+            "id":self.id,
+            "nome":self.nome,
+            "livro_id":self.livro_id,
+            "livro":self.livro.json() 
+        }
+
+class Biblioteca(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    nome = db.Column(db.String(254)) 
+    data_aquisicao = db.Column(db.String(254))
+    data_emprestimo = db.Column(db.String(254)) 
+
+    livro_id = db.Column(db.Integer, db.ForeignKey(Livro.id))
+    livro = db.relationship("Livro")
+
+    def __str__(self): 
+        s = f"Livro {self.nome} adquirido em {self.data_aquisicao}"
+        if self.livro != None:
+            s += f", foi emprestado o livro {self.livro} desde {self.data_aquisicao}"
+        return s
+
+    def json(self):
+        if self.livro is None: 
+            livro_id = ""
+            livro = ""
+            data_emprestimo = ""
+        else: 
+            livro_id = self.livro_id
+            livro = self.livro.json()
+            data_emprestimo = self.data_emprestimo
+            
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "data_aquisicao": self.data_aquisicao,
+            "livro_id": livro_id,
+            "livro": livro,
+            "data_emprestimo": data_emprestimo
+        } 
+
+
+class Leitura(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+
+    livro_id = db.Column(db.Integer, db.ForeignKey(Livro.id))
+    livro = db.relationship("Livro")
+
+    leitor_id = db.Column(db.Integer, db.ForeignKey(Leitor.id))
+    leitor_id = db.relationship("Leitor")
+
+    def __str__(self): # expressão da classe em forma de texto
+        return f"{self.livro}, {self.leitor}"
+
+    def json(self):
+        return {
+            "id":self.id,
+            "livro_id":self.livro_id,
+            "livro":self.livro.json(),
+            "leitor_id":self.leitor_id,
+            "leitor":self.leitor.json(),
+        }
+
+
+if __name__ == "__main__":
+    if os.path.exists(arquivobd):
+        os.remove(arquivobd)
+
+    db.create_all()
+
+    l1 = Livro(nome = "Cidade de papel", autor = "John Green", 
+        ano_publi = "2008")   
+    l2 = Livro(nome = "A culpa é das estrelas", autor = "John Green", 
+        ano_publi = "2012")     
+    db.session.add(l1)
+    db.session.add(l2)
+    db.session.commit()
+    
+    print(l2)
+    print(l2.json())
+    print("\n" + "\n") 
+
+    le1 = Leitor(nome="Beatriz Bauer", livro=l1)
+    db.session.add(le1)
+    db.session.commit()
+    print(f"Exame realizado: {le1}")
+    print(f"Exame realizado em json: {le1.json()}")
+    print("\n" + "\n")
+    
+    
+    a1 = Biblioteca(nome="A culpa é das estrelas", data_aquisicao="24/03/2020")
+    db.session.add(a1)
+    db.session.commit()
+    print(f"Biblioteca 1: {a1}")
+    print(f"Biblioteca 1 (em json): {a1.json()}")
+    print("\n" + "\n")
+    
+    a2 = Biblioteca(nome="Cidade de papel", data_aquisicao="01/02/2020", livro = l1, data_emprestimo="04/02/2020")
+    db.session.add(a2)
+    db.session.commit()
+    print(f"Biblioteca 2: {a2}")
+    print(f"Biblioteca 2 (em json): {a2.json()}")
+    
---
+++ modeloLary.py
@@ -0,0 +1,68 @@
+from config import *
+
+class Comida(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(254))
+    sabor = db.Column(db.String(254))
+    origem = db.Column(db.String(254))
+    dificuldade_de_preparo = db.Column(db.String(254))
+    nota = db.Column(db.Integer)
+
+    def __str__(self):
+        return self.sabor + ", " + self.origem + ", " +\
+            self.dificuldade_de_preparo + str(self.nota)
+
+    def json(self):
+        return {
+            "id": self.id,
+            "nome": self.nome,
+            "sabor": self.sabor,
+            "origem": self.origem,
+            "dificuldade_de_preparo": self.dificuldade_de_preparo,
+            "nota": self.nota,
+        }
+
+class Classificacao(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True) 
+    titulo = db.Column(db.String(254)) 
+    categoria = db.Column(db.String(254))
+    data_classificacao = db.Column(db.String(254))
+    comida_id = db.Column(db.Integer, db.ForeignKey(Comida.id))
+    comida = db.relationship("Comida")
+
+    def __str__(self): 
+        return self.titulo + ", " + self.data_classificacao +\
+            self.categoria + str(self.comida)
+
+    def json(self):
+        return ({
+            "id": self.id,
+            "titulo": self.titulo,
+            "data_classificacao": self.data_classificacao,
+            "categoria": self.categoria,
+            "comida_id": self.comida_id,
+            "comida": self.comida.json()
+        }) 
+
+class Ranking(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    posicao = db.Column(db.Integer)
+    autor = db.Column(db.String(254))
+    comida_id = db.Column(db.Integer, db.ForeignKey(Comida.id))
+    comida = db.relationship("Comida")
+
+    def __str__(self):
+        return str(self.posicao) + ", " +\
+            self.autor + str(self.comida)
+
+    def json(self):
+        return ({
+            "id":self.id,
+            "posicao":self.posicao,
+            "autor": self.autor,
+            "comida_id": self.comida_id,
+            "comida": self.comida.json()
+        })
